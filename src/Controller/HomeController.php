40c956ed8963973bc9f4f24c2d884895b3af7d15
Ylan <turin-ylan@outlook.fr>
2025-10-29 16:54:17 +0100

    remove flashes on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,9 +13,9 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $this->addFlash("danger","Test");
-        $this->addFlash("success","Test");
-        $this->addFlash("notice","Test");
+//        $this->addFlash("danger","Test");
+//        $this->addFlash("success","Test");
+//        $this->addFlash("notice","Test");
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
