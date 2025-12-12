e150518f9e815913edc4f0ac09ad065abd39df57
Ylan <turin-ylan@outlook.fr>
2025-12-12 16:33:37 +0100

    some stupid update

--- deployweb.sh
+++ deployweb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ls /var/www/html
 php /var/www/html/bin/console doctrine:database:create --if-not-exists
-php /var/www/html/bin/console doctrine:schema:update --force
+php /var/www/html/bin/console doctrine:schema:update --force --dump-sql
 composer install
 ln -sf /etc/nginx/sites-available/website.conf /etc/nginx/sites-enabled/website.conf
 rm -f /etc/nginx/sites-enabled/default
 chown -R www-data:www-data /var/www/html /var/log/nginx
-ln -sf /dev/stdout /var/log/nginx/access.log 
+ln -sf /dev/stdout /var/log/nginx/access.log
 ln -sf /dev/stderr /var/log/nginx/error.log
