0fd354150cdc83d446a76ceac05a17af04ec1d13
Ylan <turin-ylan@outlook.fr>
2025-12-18 08:50:01 +0100

    update PointCrudController to include user association, reason, date, and points fields

--- src/Controller/Admin/PointCrudController.php
+++ src/Controller/Admin/PointCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Point;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,17 @@ class PointCrudController extends AbstractCrudController
         return Point::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('user', 'Utilisateur'),
+            TextField::new('reason', 'Raison'),
+            DateTimeField::new('date', 'Date')->formatValue(function ($value) {
+                return $value ? $value->format('d/m/Y H:i') : '';
+            }),
+            NumberField::new('points', 'Points'),
         ];
     }
-    */
 }
