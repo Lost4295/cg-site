c152cb390457e2a30263b7aacac35190b848e5e1
Ylan <turin-ylan@outlook.fr>
2026-02-04 23:26:59 +0100

    fix : update point calculations and display in point detail view

--- src/Controller/PointController.php
+++ src/Controller/PointController.php
@@ -95,24 +95,21 @@ final class PointController extends AbstractController
         $userPoints[2]['o'] = 0;
         $userPoints[3]['o'] = 0;
 
-        $dates = [];
+        $dates = [1 => [], 2 => [], 3 => []];
         for ($i = 1; $i <= 3; $i++) {
             foreach ($userPoints[$i]['points'] as $point) {
-                if ($point->getReason() == "Présence à l'association en présentiel") {
-                    $userPoints[$i]['p']++;
-                    $dates[$i][] = $point->getDate()->format('d/m/Y');
+                if (in_array($point->getReason(), [ "Présence à l'association en présentiel", "Bonne réponse", "Mauvaise réponse", "Image acceptée"])) {
+                    $userPoints[$i]['p']+= $point->getPoints();
+                    $dates[$i][] = ['date' =>$point->getDate()->format('d/m/Y'), 'points'=>$point->getPoints()];
                 } else {
-                    $userPoints[$i]['o']++;
+                    $userPoints[$i]['o']+= $point->getPoints();
                 }
             }
         }
-        $userPoints[1]['p'] = min($userPoints[1]['p'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-        $userPoints[2]['p'] = min($userPoints[2]['p'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-        $userPoints[3]['p'] = min($userPoints[3]['p'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-        $userPoints[1]['o'] = min($userPoints[1]['o'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-        $userPoints[2]['o'] = min($userPoints[2]['o'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-        $userPoints[3]['o'] = min($userPoints[3]['o'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
-
+        $userPoints[1]['realTotal'] = min($userPoints[1]['total'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
+        $userPoints[2]['realTotal'] = min($userPoints[2]['total'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
+        $userPoints[3]['realTotal'] = min($userPoints[3]['total'], $user->getIsAdmin() == 0 ? 4 : ($user->getIsAdmin() == 1 ? 6 : 8));
+        dump($userPoints, $dates);
         return $this->render('point/point_detail.html.twig', [
             'point' => $userPoints,
             'dates' => $dates
